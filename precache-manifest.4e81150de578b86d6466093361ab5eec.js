6dd96c2403f06d31ab3798c4e410c7cc3add389d
alex <boomscrip@gmail.com>
2019-04-04 10:12:20 +0300

    Updates

---
+++ precache-manifest.4e81150de578b86d6466093361ab5eec.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "fb2f5c45ea1831de56dd",
+    "url": "/forivan/static/css/main.5e8145f4.chunk.css"
+  },
+  {
+    "revision": "fb2f5c45ea1831de56dd",
+    "url": "/forivan/static/js/main.c9b1012b.chunk.js"
+  },
+  {
+    "revision": "56ece4aa971476e7b0a5",
+    "url": "/forivan/static/js/runtime~main.b6369e97.js"
+  },
+  {
+    "revision": "b66c59bdc47e493aa5f0",
+    "url": "/forivan/static/css/2.8cae89d7.chunk.css"
+  },
+  {
+    "revision": "b66c59bdc47e493aa5f0",
+    "url": "/forivan/static/js/2.acc95e19.chunk.js"
+  },
+  {
+    "revision": "1e8fb2633f1746096f1d8e11f6e8d023",
+    "url": "/forivan/index.html"
+  }
+];
